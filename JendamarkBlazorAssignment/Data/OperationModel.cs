e783c14cebccd9390fa1349c91da4a228feaf9be
bean1352 <72529945+bean1352@users.noreply.github.com>
2022-02-04 23:56:51 +0200

    First commit

---
+++ JendamarkBlazorAssignment/Data/OperationModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JendamarkBlazorAssignment.Models
+{
+    public class Operation
+    {
+        public Guid OperationID { get; set; }
+        public string Name { get; set; }
+        public int OrderInWhichToPerform { get; set; }
+        public byte[] ImageData { get; set; }
+        public Device Device { get; set; }
+        public string ImageDataUrl { get; set; }
+    }
+    public class Device
+    {
+        private DeviceType _devicetype;
+        public Guid DeviceID { get; set; }
+        public string Name { get; set; }
+        public DeviceType DeviceType
+        {
+            get { return _devicetype; }
+            set { _devicetype = value; }
+        }
+    }
+    public enum DeviceType
+    {
+        BarcodeScanner,
+        Printer,
+        Camera,
+        SocketTray
+    }
+}
---
+++ JendamarkBlazorAssignment/Data/OperationService.cs
@@ -0,0 +1,49 @@
+﻿using JendamarkBlazorAssignment.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JendamarkBlazorAssignment.Data
+{
+    public class OperationService
+    {
+        string[] operationNames = { "Steel Operation", "Welding Operation", "Surgical Operation", "Build Operation",
+                            "Assemble Operation" };
+        public string[] deviceNames = { "HP", "LG", "Samsung", "Apple", "Motorolla" };
+
+        private List<byte[]> GetImages()
+        {
+            string[] imageNames = { "steel.png", "welding.png", "surgery.png", "build.png", "assemble.png"};
+
+            string workingDirectory = Environment.CurrentDirectory;
+
+            List<byte[]> list = new();
+
+            for (int i = 0; i < imageNames.Length; i++)
+            {
+                list.Add(File.ReadAllBytes($"{workingDirectory}\\Images\\{imageNames[i]}"));
+            }
+
+            return list;
+        }
+
+        public Task<Operation[]> GetOperationAsync()
+        {
+            List<byte[]> imageList = GetImages();
+
+            var rng = new Random();
+            return Task.FromResult(Enumerable.Range(0, 4).Select(index => new Operation
+            {
+                OperationID = Guid.NewGuid(),
+                Name = operationNames[index],
+                Device = new Device() { Name = deviceNames[index], DeviceID = Guid.NewGuid(), DeviceType = DeviceType.Camera},
+                OrderInWhichToPerform = index,
+                ImageData = imageList[index],
+                ImageDataUrl = string.Format("data:image/jpeg;base64,{0}", Convert.ToBase64String(imageList[index]))
+            }).ToArray());
+        }
+    }
+}
